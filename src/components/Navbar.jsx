6b6182841e46d2c25e33f1db090e67db6ffab47b
ArlaTab <123273102+ArlaTab@users.noreply.github.com>
2024-07-23 11:20:18 -0600

    add pictures and function

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navb"></nav>
+      <div className="navbar-container">
+        
+      </div>
+    </>
+  )
+}
+
+export default Navbar
