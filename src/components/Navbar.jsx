cb29751c7fa5ae38e13a38be8339dcd78c838fce
ArlaTab <123273102+ArlaTab@users.noreply.github.com>
2024-07-30 09:23:56 -0600

    add scissors icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,11 +12,14 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to ="/" className="navbar-logo">
-            DR. TRIM <i className="fab fa-typo3" />
+            DR. TRIM <i className="fa-solid fa-scissors" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            
+          </ul>
         </div>
       </nav>
     </>
