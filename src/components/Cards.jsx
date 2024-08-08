c3db387ddc2f6dd8684ae696e6eb91b8037c5c8c
ArlaTab <123273102+ArlaTab@users.noreply.github.com>
2024-08-08 14:46:36 -0600

    make more tabs

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -17,6 +17,32 @@ function Cards() {
                 label='Previous Work'
                 path='/services'
                />
+               <CardItem 
+                src={require('../images/img-4.jpeg')}
+                text='See our Before and After Work'
+                label='Before and After'
+                path='/services'
+               />
+            </ul>
+            <ul className='cards__items'>
+               <CardItem 
+                src={require('../images/img-3.jpeg')}
+                text='Contact us to schedule an Appointment'
+                label='Appointment'
+                path='/services'
+               />
+               <CardItem 
+                src={require('../images/img-5.jpeg')}
+                text='We travel to you'
+                label='Mobile'
+                path='/services'
+               />
+               <CardItem 
+                src={require('../images/img-home.jpeg')}
+                text='More than 15 years experience'
+                label='Speciality Work'
+                path='/services'
+               />
             </ul>
         </div>
       </div>
