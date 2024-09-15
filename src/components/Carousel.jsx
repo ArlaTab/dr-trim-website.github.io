6daf17c3d392778e5c039cf49a365d1350410950
ArlaTab <123273102+ArlaTab@users.noreply.github.com>
2024-09-15 12:28:40 -0600

    jpeg to jpg for amplify

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -58,27 +58,27 @@ const SampleNextArrow = (props) => {
       <h2>Our Products- Before and After</h2>
       <Slider {...settings}>
         <div className="carousel__item">
-          <img src={require('../images/tesla_before.jpeg')} alt="Item tesla" />
+          <img src={require('../images/tesla_before.jpg')} alt="Item tesla" />
           <p>Before- Tesla Steering Wheel </p>
         </div>
         <div className="carousel__item">
-          <img src={require('../images/tesla_after.jpeg')} />
+          <img src={require('../images/tesla_after.jpg')} alt="Item tesla2" />
           <p>After- Tesla Steering Wheel </p>
         </div>
         <div className="carousel__item">
-          <img src={require('../images/img-1.jpeg')} alt="Item blackcouch" />
+          <img src={require('../images/img-1.jpg')} alt="Item blackcouch" />
           <p>Before-Leather Couch Damage</p>
         </div>
         <div className="carousel__item">
-          <img src={require('../images/img-1part2.jpeg')} alt="Item blackcouch2" />
+          <img src={require('../images/img-1part2.jpg')} alt="Item blackcouch2" />
           <p>After-Leather Couch Damage </p>
         </div>
         <div className="carousel__item">
-          <img src={require('../images/img-4.jpeg')} alt="Carseat" />
+          <img src={require('../images/img-4.jpg')} alt="Carseat" />
           <p>Before- Car Seat </p>
         </div>
         <div className="carousel__item">
-          <img src={require('../images/img-4part2.jpeg')} alt="Carseat2" />
+          <img src={require('../images/img-4part2.jpg')} alt="Carseat2" />
           <p>After- Car Seat </p>
         </div>
         {/* Add more items as needed */}
